5a46961c206ec27e3a9f6592f48ad1e3ba64bae6
jupiter8nohate <joehud777@gmail.com>
2026-09-04 09:51:57 -0400

    feat(cmb-edu): export FGC and commitment APIs

--- src/cmb_edu/__init__.py
+++ src/cmb_edu/__init__.py
@@ -1,6 +1,16 @@
 """CMB-EDU: privacy-first metacognitive literacy primitives."""
 
+from .fgc import FGCEmojiParser
+from .models import ContextEnvelope, PrivacyPolicy
 from .parser import CMBDualBrainParser, CMBParseError
+from .provenance import build_context_commitment
 
-__all__ = ["CMBDualBrainParser", "CMBParseError"]
+__all__ = [
+    "CMBDualBrainParser",
+    "CMBParseError",
+    "ContextEnvelope",
+    "FGCEmojiParser",
+    "PrivacyPolicy",
+    "build_context_commitment",
+]
 __version__ = "0.1.0"
