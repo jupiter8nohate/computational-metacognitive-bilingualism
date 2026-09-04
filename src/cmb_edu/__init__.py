6e26007d8739f2bbbda68a1dbe4cf4360dc9343e
jupiter8nohate <joehud777@gmail.com>
2026-09-04 09:42:26 -0400

    feat(cmb-edu): add metacognitive education layer

---
+++ src/cmb_edu/__init__.py
@@ -0,0 +1,6 @@
+"""CMB-EDU: privacy-first metacognitive literacy primitives."""
+
+from .parser import CMBDualBrainParser, CMBParseError
+
+__all__ = ["CMBDualBrainParser", "CMBParseError"]
+__version__ = "0.1.0"
