16b91fc1989dd96231aaac4e9bfff074abde1869
jupiter8nohate <joehud777@gmail.com>
2026-09-04 13:35:50 -0400

    Expose HARMONI discovery ladder API

--- src/cmb_machine/__init__.py
+++ src/cmb_machine/__init__.py
@@ -2,16 +2,20 @@
 
 from .compiler import MachineArtifact, compile_bundle, render_target, supported_targets
 from .harmoni import (
+    DiscoveryStage,
     EpistemicState,
     HarmoniDecision,
     HarmoniLayer,
     ProofGate,
     evaluate_claim,
+    next_discovery_stage,
+    validate_discovery_transition,
     harmoni_manifest,
 )
 from .ir import build_core_ir, normalize_ir
 
 __all__ = [
+    "DiscoveryStage",
     "EpistemicState",
     "HarmoniDecision",
     "HarmoniLayer",
@@ -21,9 +25,11 @@ __all__ = [
     "compile_bundle",
     "evaluate_claim",
     "harmoni_manifest",
+    "next_discovery_stage",
     "normalize_ir",
     "render_target",
     "supported_targets",
+    "validate_discovery_transition",
 ]
 
 __version__ = "0.2.0"
