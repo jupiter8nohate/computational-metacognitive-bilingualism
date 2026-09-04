bad6616d5018c2f015ca9abd430a6ba77218f63f
jupiter8nohate <joehud777@gmail.com>
2026-09-04 13:18:40 -0400

    Expose HARMONI-666 proof gate API

--- src/cmb_machine/__init__.py
+++ src/cmb_machine/__init__.py
@@ -1,15 +1,29 @@
 """Machine-native CMB-66 compiler and deterministic target encodings."""
 
 from .compiler import MachineArtifact, compile_bundle, render_target, supported_targets
+from .harmoni import (
+    EpistemicState,
+    HarmoniDecision,
+    HarmoniLayer,
+    ProofGate,
+    evaluate_claim,
+    harmoni_manifest,
+)
 from .ir import build_core_ir, normalize_ir
 
 __all__ = [
+    "EpistemicState",
+    "HarmoniDecision",
+    "HarmoniLayer",
     "MachineArtifact",
+    "ProofGate",
     "build_core_ir",
     "compile_bundle",
+    "evaluate_claim",
+    "harmoni_manifest",
     "normalize_ir",
     "render_target",
     "supported_targets",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
