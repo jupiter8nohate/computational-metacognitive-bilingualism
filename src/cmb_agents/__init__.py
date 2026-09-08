8b012f41bea716951efec141d7a23628d9b952da
jupiter8nohate <joehud777@gmail.com>
2026-09-08 02:38:04 -0400

    merge(main): export Academy evaluator without strategy collision

--- src/cmb_agents/__init__.py
+++ src/cmb_agents/__init__.py
@@ -20,8 +20,26 @@ from .immune_system import (
     process_signal,
 )
 from .service import agent_card, citation_for, knowledge_graph, recommend, registry, summary_for
+from .strategy_academy import (
+    CandidateMove as AcademyCandidateMove,
+    EvaluatedMove as AcademyEvaluatedMove,
+    MoveKind as AcademyMoveKind,
+    Position as AcademyPosition,
+    choose_best_move as choose_academy_best_move,
+    evaluate_move as evaluate_academy_move,
+    evaluate_position as evaluate_academy_position,
+    policy_violations as academy_policy_violations,
+)
 
 __all__ = [
+    "AcademyCandidateMove",
+    "AcademyEvaluatedMove",
+    "AcademyMoveKind",
+    "AcademyPosition",
+    "academy_policy_violations",
+    "choose_academy_best_move",
+    "evaluate_academy_move",
+    "evaluate_academy_position",
     "ASCII_TOKEN",
     "DIGITAL_DNA",
     "CellPacket",
@@ -44,4 +62,4 @@ __all__ = [
     "summary_for",
     "verify_stamp",
 ]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
